6149973ea9e30bc7d6ee656de53ccb44bad6a0c9
mjsmccarthy <mjsmccarthy@gmail.com>
2019-07-29 19:48:36 -0400

    Refactored using object shorthand/destructuring

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/72d02ad80c5e7fd33e40c2b50bc78c2a/' + latitude + ',' + longitude
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            let temp = response.body.currently.temperature
-            let precip = response.body.currently.precipProbability
-            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + temp + ' degrees out. There is a ' + precip + '% chance of rain.')
+            let temp = body.currently.temperature
+            let precip = body.currently.precipProbability
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + temp + ' degrees out. There is a ' + precip + '% chance of rain.')
         }
     })
 }
