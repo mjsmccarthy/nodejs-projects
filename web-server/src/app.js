08e273b87fd622b6bddf8fcc3d98a17831948ad0
mjmccar <mccarthy.matthew@vast-inc.com>
2019-07-30 08:12:31 -0400

    Setup handlebars templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,35 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
+
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Setup handlebars engine
+app.set('view engine', 'hbs')
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Matthew'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Matthew'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'Test help message.'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
