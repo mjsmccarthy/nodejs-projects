13fb4e68e138dff10858b7b05ef5b673bbbcd2f3
mjsmccarthy <mjsmccarthy@gmail.com>
2019-07-29 20:47:42 -0400

    Added web-server functionality

---
+++ web-server/src/app.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.send('<h1>Weather</h1>')
+})
+
+app.get('/help', (req, res) => {
+    res.send([{
+        name: 'Andrew'
+    }, {
+        name: 'Matthew'
+    }])
+})
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About</h1>')
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It\'s hot',
+        location: 'California'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
